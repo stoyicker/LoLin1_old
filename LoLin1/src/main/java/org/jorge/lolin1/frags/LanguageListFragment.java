b3cb7e5cf052abfd2a7b99fce9b804b2baa7c8ea
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-03-21 15:38:22 +0100

    The implementation of the adaptive generator of the languages for each realm is almost finished.

--- LoLin1/src/main/java/org/jorge/lolin1/frags/LanguageListFragment.java
+++ LoLin1/src/main/java/org/jorge/lolin1/frags/LanguageListFragment.java
@@ -2,7 +2,10 @@ package org.jorge.lolin1.frags;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.util.Xml;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -30,6 +33,7 @@ import org.jorge.lolin1.utils.LoLin1Utils;
 public class LanguageListFragment extends Fragment {
 
     private LanguageListFragmentListener mCallback;
+    private LanguageListControlledView controlledView;
 
     /**
      * Called when the hidden state (as returned by {@link #isHidden()} of
@@ -42,28 +46,50 @@ public class LanguageListFragment extends Fragment {
     @Override
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
-        if (hidden) {
-            ViewGroup viewAsViewGroup = (ViewGroup) getView();
-            viewAsViewGroup.removeAllViews();
-            String realm_composite =
-                    new StringBuilder(LoLin1Utils.getString(getActivity().getApplicationContext(),
-                            "realm_to_language_list_prefix", "lang_euw"))
-                            .append(
-                                    mCallback.onCurrentlySelectedRealmRequest().toLowerCase())
-                            .toString();
-            String[] languages =
-                    LoLin1Utils
-                            .getStringArray(getActivity().getApplicationContext(), realm_composite,
-                                    new String[]{"NO_LANGUAGE_FOUND"});
-            for (String language : languages) {
-                TextView textView =
-                        new TextView(getActivity().getApplicationContext(),
-                                Xml.asAttributeSet(getResources().getXml(
-                                        R.xml.server_and_language_text_views_attribute_set))
+        if (!hidden) {
+            reloadLanguages();
+        }
+    }
+
+    private void reloadLanguages() {
+        ViewGroup viewAsViewGroup = (ViewGroup) getView();
+        viewAsViewGroup.removeAllViews();
+        String realm_composite =
+                new StringBuilder(LoLin1Utils.getString(getActivity().getApplicationContext(),
+                        "realm_to_language_list_prefix", "lang_"))
+                        .append(
+                                mCallback.onCurrentlySelectedRealmRequest().toLowerCase())
+                        .toString();
+        String[] languages =
+                LoLin1Utils
+                        .getStringArray(getActivity().getApplicationContext(), realm_composite,
+                                new String[]{"NO_LANGUAGE_FOUND"}), languages_simplified =
+                LoLin1Utils
+                        .getStringArray(getActivity().getApplicationContext(),
+                                new StringBuilder(realm_composite).append(LoLin1Utils
+                                        .getString(getActivity().getApplicationContext(),
+                                                "language_to_simplified_suffix", "_simplified"))
+                                        .toString(),
+                                new String[]{"NO_LANGUAGE_FOUND"}
                         );
-                textView.setText(language);
-                viewAsViewGroup.addView(textView);
-            }
+        int languageCounter = 0;
+        for (String language : languages) {
+            TextView textView =
+                    new TextView(getActivity().getApplicationContext(),
+                            Xml.asAttributeSet(getResources().getXml(
+                                    R.xml.server_and_language_text_views_attribute_set))
+                    ) {
+                        @Override
+                        public boolean onTouchEvent(MotionEvent event) {
+                            mCallback.onLocaleSelected(/*TODO Calculate selected locale*/);
+                            /*TODO Highlight this TextView with the ServerAndLanguageChooserUnselectedStyle - http://stackoverflow.com/questions/11723881/android-set-view-style-programatically*/
+                            return Boolean.TRUE;
+                        }
+                    };
+            textView.setText(language);
+            textView.setTag(languages_simplified[languageCounter]);
+            viewAsViewGroup.addView(textView);
+            languageCounter++;
         }
     }
 
@@ -81,7 +107,7 @@ public class LanguageListFragment extends Fragment {
 
     public void notifyNewRealmHasBeenSelected(String newSelectedRealm) {
         mCallback.updateLanguageChooserVisibility();
-        //TODO notifyNewRealmHasBeenSelected
+        reloadLanguages();
     }
 
     public interface LanguageListFragmentListener {
@@ -90,5 +116,17 @@ public class LanguageListFragment extends Fragment {
         public String onCurrentlySelectedRealmRequest();
 
         public void updateLanguageChooserVisibility();
+    }
+
+    private class LanguageListControlledView extends View {
+        /**
+         * Simple constructor to use when creating a view from code.
+         *
+         * @param context The Context the view is running in, through which it can
+         *                access the current theme, resources, etc.
+         */
+        public LanguageListControlledView(Context context) {
+            super(context);
+        }
     }
 }
