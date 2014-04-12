c738cef3011d9bdc61dbf17af7c919762640a005
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-04-13 01:22:51 +0200

    Tiny repository cleanse.

--- LoLin1/src/main/java/org/jorge/lolin1/ui/frags/SurrListFragment.java
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/frags/SurrListFragment.java
@@ -79,20 +79,6 @@ public class SurrListFragment extends ListFragment implements OnRefreshListener 
 
         if (LoLin1Utils.isInternetReachable(getActivity())) {
             new AsyncTask<Void, Void, Void>() {
-                /**
-                 * Override this method to perform a computation on a background thread. The
-                 * specified parameters are the parameters passed to {@link #execute}
-                 * by the caller of this task.
-                 * <p/>
-                 * This method can call {@link #publishProgress} to publish updates
-                 * on the UI thread.
-                 *
-                 * @param params The parameters of the task.
-                 * @return A result, defined by the subclass of this task.
-                 * @see #onPreExecute()
-                 * @see #onPostExecute
-                 * @see #publishProgress
-                 */
                 @Override
                 protected Void doInBackground(Void... params) {
                     //It is "not possible" that the user comes back from the
