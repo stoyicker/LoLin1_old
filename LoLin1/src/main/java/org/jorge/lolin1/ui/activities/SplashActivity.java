2d2fee3140e93ba7b58d367beadf17075e428c44
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-04-12 17:46:41 +0200

    Bugfixing and refactoring.

--- LoLin1/src/main/java/org/jorge/lolin1/ui/activities/SplashActivity.java
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/activities/SplashActivity.java
@@ -53,7 +53,10 @@ public class SplashActivity extends Activity {
                         new SmoothProgressDrawable.Builder(getApplicationContext())
                                 .color(getApplicationContext().getResources()
                                         .getColor(R.color.theme_strong_orange))
-                                .interpolator(new AccelerateDecelerateInterpolator()).build()
+                                .interpolator(new AccelerateDecelerateInterpolator()).speed(Float.parseFloat(LoLin1Utils.getString(getApplicationContext(),"splash_progress_bar_speed",null)))
+                                .sectionsCount(LoLin1Utils.getInt(getApplicationContext(),
+                                        "splash_progress_bar_sections", -1))
+                                .build()
                 );
 
         LOG_FRAGMENT =
@@ -65,37 +68,13 @@ public class SplashActivity extends Activity {
 
     private void load() {
         new AsyncTask<Void, Integer, Void>() {
-            /**
-             * <p>Runs on the UI thread after {@link #doInBackground}. The
-             * specified result is the value returned by {@link #doInBackground}.</p>
-             * <p/>
-             * <p>This method won't be invoked if the task was cancelled.</p>
-             *
-             * @param aVoid The result of the operation computed by {@link #doInBackground}.
-             * @see #onPreExecute
-             * @see #doInBackground
-             * @see #onCancelled(Object)
-             */
+
             @Override
             protected void onPostExecute(Void aVoid) {
                 super.onPostExecute(aVoid);
                 launchNewsReader();
             }
 
-            /**
-             * Override this method to perform a computation on a background thread. The
-             * specified parameters are the parameters passed to {@link #execute}
-             * by the caller of this task.
-             * <p/>
-             * This method can call {@link #publishProgress} to publish updates
-             * on the UI thread.
-             *
-             * @param params The parameters of the task.
-             * @return A result, defined by the subclass of this task.
-             * @see #onPreExecute()
-             * @see #onPostExecute
-             * @see #publishProgress
-             */
             @Override
             protected Void doInBackground(Void... params) {
                 final CountDownLatch countDownLatch = new CountDownLatch(2);
