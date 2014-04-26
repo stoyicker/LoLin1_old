40bd76b1e3dd7cb91b0e8f6758a391c67daa191a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-04-26 14:26:46 +0200

    Bugfixing.

--- LoLin1/src/main/java/org/jorge/lolin1/ui/activities/SettingsPreferenceActivity.java
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/activities/SettingsPreferenceActivity.java
@@ -25,18 +25,6 @@ import org.jorge.lolin1.utils.LoLin1Utils;
  * <p/>
  * Created by JorgeAntonio on 13/01/14.
  */
-
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p/>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
 public final class SettingsPreferenceActivity extends PreferenceActivity {
 
     @Override
--- LoLin1/src/main/java/org/jorge/lolin1/ui/frags/SettingsFragment.java
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/frags/SettingsFragment.java
@@ -6,8 +6,10 @@ import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.jorge.lolin1.R;
+import org.jorge.lolin1.utils.LoLin1DebugUtils;
 import org.jorge.lolin1.utils.LoLin1Utils;
 
 import java.util.ArrayList;
@@ -43,6 +45,24 @@ public class SettingsFragment extends PreferenceFragment {
                 LoLin1Utils.getString(getActivity().getApplicationContext(), "pref_title_server",
                         null)
         );
+
+        String[] targetArray;
+        String chosenServer;
+        chosenServer = LoLin1Utils.getRealm(getActivity().getApplicationContext());
+        Log.d("debug", "realm: " + chosenServer);
+        serverPreference.setValue(chosenServer);
+        targetArray = LoLin1Utils
+                .getStringArray(getActivity().getApplicationContext(),
+                        "lang_" + chosenServer.toLowerCase(), null);
+        final ListPreference langPreference = (ListPreference) findPreference(
+                LoLin1Utils.getString(getActivity(), "pref_title_lang",
+                        null)
+        );
+        Log.d("debug", "Entries set: ");
+        LoLin1DebugUtils.logArray("debug", "targetArray", targetArray);
+        langPreference.setEntries(targetArray);
+        langPreference.setEntryValues(targetArray);
+
         serverPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -79,9 +99,16 @@ public class SettingsFragment extends PreferenceFragment {
             }
         });
 
-        final ListPreference langPreference = (ListPreference) findPreference(
-                LoLin1Utils.getString(getActivity(), "pref_title_lang", null)
-        );
+        String currentLocale = LoLin1Utils.getLocale(getActivity().getApplicationContext());
+        Log.d("debug", "locale: " + currentLocale);
+        int langIndex =
+                new ArrayList<>(Arrays.asList(
+                        LoLin1Utils.getStringArray(getActivity(), "langs_simplified", null)))
+                        .indexOf(currentLocale);
+        Log.d("debug", "langIndex " + langIndex);
+        langPreference
+                .setValue(LoLin1Utils.getStringArray(getActivity(), "langs", null)[langIndex]);
+
         langPreference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference preference, Object newValue) {
