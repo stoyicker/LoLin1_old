5e2fb5c7855db9445e947e744a789873e7e82a56
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-05-01 19:22:39 +0200

    Added title to the login authenticator.

---
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/frags/LoLChatAuthenticatorTitleFragment.java
@@ -0,0 +1,37 @@
+package org.jorge.lolin1.ui.frags;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.jorge.lolin1.R;
+
+/**
+ * This file is part of LoLin1.
+ * <p/>
+ * LoLin1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * LoLin1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with LoLin1. If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Created by JorgeAntonio on 01/05/2014.
+ */
+public class LoLChatAuthenticatorTitleFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View ret = inflater.inflate(R.layout.fragment_chat_authenticator_title, container, false);
+        return ret;
+    }
+}
