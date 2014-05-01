a806e37fa2410f483f64ce974842659aca863b7a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-05-01 19:51:04 +0200

    Added the realm selector to the authenticator layout.

---
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/frags/AuthRealmSelectorFragment.java
@@ -0,0 +1,46 @@
+package org.jorge.lolin1.ui.frags;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import org.jorge.lolin1.R;
+import org.jorge.lolin1.utils.LoLin1Utils;
+
+import java.util.Arrays;
+
+/**
+ * This file is part of LoLin1.
+ * <p/>
+ * LoLin1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * LoLin1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with LoLin1. If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Created by JorgeAntonio on 01/05/2014.
+ */
+public class AuthRealmSelectorFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View ret =
+                inflater.inflate(R.layout.fragment_authenticator_realm_selector, container, false);
+        Spinner spinner = (Spinner) ret.findViewById(R.id.authenticator_realm_selector_spinner);
+        spinner.setSelection(Arrays.asList(
+                LoLin1Utils.getStringArray(getActivity().getApplicationContext(), "servers", null))
+                .indexOf(LoLin1Utils.getRealm(getActivity().getApplicationContext())));
+        return ret;
+    }
+}
