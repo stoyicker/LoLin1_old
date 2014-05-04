3c72dd1fbbcce5cb0c3df3d753f384a044f62934
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-05-04 16:53:35 +0200

    Added a method to allow the Friend-type objects to be filtered by name.

--- LoLChatAPI/src/main/java/com/github/theholywaffle/lolchatapi/wrapper/Friend.java
+++ LoLChatAPI/src/main/java/com/github/theholywaffle/lolchatapi/wrapper/Friend.java
@@ -10,7 +10,10 @@
  ******************************************************************************/
 package com.github.theholywaffle.lolchatapi.wrapper;
 
-import java.io.IOException;
+import com.github.theholywaffle.lolchatapi.ChatMode;
+import com.github.theholywaffle.lolchatapi.LolChat;
+import com.github.theholywaffle.lolchatapi.LolStatus;
+import com.github.theholywaffle.lolchatapi.listeners.ChatListener;
 
 import org.jdom2.JDOMException;
 import org.jivesoftware.smack.Chat;
@@ -22,166 +25,170 @@ import org.jivesoftware.smack.packet.Message;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.packet.Presence.Type;
 
-import com.github.theholywaffle.lolchatapi.ChatMode;
-import com.github.theholywaffle.lolchatapi.LolChat;
-import com.github.theholywaffle.lolchatapi.LolStatus;
-import com.github.theholywaffle.lolchatapi.listeners.ChatListener;
+import java.io.IOException;
 
 /**
  * This and all the files in the module have been developed by Bert De Geyter (https://github.com/TheHolyWaffle) and are protected by the Apache GPLv3 license.
  */
 public class Friend extends Wrapper<RosterEntry> {
 
-	private Friend instance = null;
-	private Chat chat = null;
-	private ChatListener listener = null;
+    private Friend instance = null;
+    private Chat chat = null;
+    private ChatListener listener = null;
 
-	public Friend(LolChat api, XMPPConnection connection, RosterEntry entry) {
-		super(api, connection, entry);
-		this.instance = this;
-	}
+    public Friend(LolChat api, XMPPConnection connection, RosterEntry entry) {
+        super(api, connection, entry);
+        this.instance = this;
+    }
 
-	/**
-	 * Deletes this friend.
-	 *
-	 * @return true if succesful, otherwise false
-	 */
-	public boolean delete() {
-		try {
-			con.getRoster().removeEntry(get());
-			return true;
-		} catch (XMPPException e) {
-			e.printStackTrace();
-		}
-		return false;
-	}
+    /**
+     * Deletes this friend.
+     *
+     * @return true if succesful, otherwise false
+     */
+    public boolean delete() {
+        try {
+            con.getRoster().removeEntry(get());
+            return true;
+        }
+        catch (XMPPException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
-	private Chat getChat() {
-		if (chat == null) {
-			chat = con.getChatManager().createChat(getUserId(),
-					new MessageListener() {
+    private Chat getChat() {
+        if (chat == null) {
+            chat = con.getChatManager().createChat(getUserId(),
+                    new MessageListener() {
 
-						@Override
-						public void processMessage(Chat c, Message m) {
-							if (chat != null && listener != null) {
-								listener.onMessage(instance, m.getBody());
-							}
+                        @Override
+                        public void processMessage(Chat c, Message m) {
+                            if (chat != null && listener != null) {
+                                listener.onMessage(instance, m.getBody());
+                            }
 
-						}
-					});
-		}
-		return chat;
-	}
+                        }
+                    }
+            );
+        }
+        return chat;
+    }
 
-	/**
-	 * Returns the current ChatMode of this friend (e.g. away, busy,
-	 * available)
-	 *
-	 * @see ChatMode
-	 * @return ChatMode of this friend
-	 */
-	public ChatMode getChatMode() {
-		Presence.Mode mode = con.getRoster().getPresence(getUserId()).getMode();
-		for (ChatMode c : ChatMode.values()) {
-			if (c.mode == mode) {
-				return c;
-			}
-		}
-		return null;
-	}
+    /**
+     * Returns the current ChatMode of this friend (e.g. away, busy,
+     * available)
+     *
+     * @return ChatMode of this friend
+     * @see ChatMode
+     */
+    public ChatMode getChatMode() {
+        Presence.Mode mode = con.getRoster().getPresence(getUserId()).getMode();
+        for (ChatMode c : ChatMode.values()) {
+            if (c.mode == mode) {
+                return c;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * @return the FriendGroup that currently contains this Friend
-	 */
-	public FriendGroup getGroup() {
-		return new FriendGroup(api, con, get().getGroups().iterator().next());
-	}
+    /**
+     * @return the FriendGroup that currently contains this Friend
+     */
+    public FriendGroup getGroup() {
+        return new FriendGroup(api, con, get().getGroups().iterator().next());
+    }
 
-	/**
-	 * @return name of your Friend (e.g. Dyrus)
-	 */
-	public String getName() {
-		return get().getName();
-	}
+    /**
+     * @return name of your Friend (e.g. Dyrus)
+     */
+    public String getName() {
+        return get().getName();
+    }
 
-	/**
-	 * Returns Status object that contains all data when hovering over a friend
-	 * inside League of Legends client (e.g. amount of normal wins, current
-	 * division and league, queue name, gamestatus,...)
-	 *
-	 * @return Status
-	 */
-	public LolStatus getStatus() {
-		String status = con.getRoster().getPresence(getUserId()).getStatus();
-		if (status != null && !status.isEmpty()) {
-			try {
-				return new LolStatus(status);
-			} catch (JDOMException | IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return new LolStatus();
-	}
+    /**
+     * Returns Status object that contains all data when hovering over a friend
+     * inside League of Legends client (e.g. amount of normal wins, current
+     * division and league, queue name, gamestatus,...)
+     *
+     * @return Status
+     */
+    public LolStatus getStatus() {
+        String status = con.getRoster().getPresence(getUserId()).getStatus();
+        if (status != null && !status.isEmpty()) {
+            try {
+                return new LolStatus(status);
+            }
+            catch (JDOMException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return new LolStatus();
+    }
 
-	/**
-	 * @return the XMPPAddress of your Friend (e.g. sum123456@pvp.net)
-	 */
-	public String getUserId() {
-		return get().getUser();
-	}
+    /**
+     * @return the XMPPAddress of your Friend (e.g. sum123456@pvp.net)
+     */
+    public String getUserId() {
+        return get().getUser();
+    }
 
-	/**
-	 * Returns true if this friend is online.
-	 *
-	 * @return true if online, false if offline
-	 */
-	public boolean isOnline() {
-		return con.getRoster().getPresence(getUserId()).getType() == Type.available;
-	}
+    /**
+     * Returns true if this friend is online.
+     *
+     * @return true if online, false if offline
+     */
+    public boolean isOnline() {
+        return con.getRoster().getPresence(getUserId()).getType() == Type.available;
+    }
 
-	/**
-	 * Sends a message to this friend
-	 *
-	 */
-	public void sendMessage(String message) {
-		try {
-			getChat().sendMessage(message);
-		} catch (XMPPException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Sends a message to this friend
+     */
+    public void sendMessage(String message) {
+        try {
+            getChat().sendMessage(message);
+        }
+        catch (XMPPException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Sends a message to this friend and sets the ChatListener. Only 1
-	 * ChatListener can be active for each Friend. Any previously set
-	 * ChatListener will be replaced by this one.
-	 *
-	 * This ChatListener gets called when this Friend only sends you a message.
-	 *
-	 * @param message
-	 * @param listener
-	 *            Your new active ChatListener
-	 */
-	public void sendMessage(String message, ChatListener listener) {
-		this.listener = listener;
-		try {
-			getChat().sendMessage(message);
-		} catch (XMPPException e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * Sends a message to this friend and sets the ChatListener. Only 1
+     * ChatListener can be active for each Friend. Any previously set
+     * ChatListener will be replaced by this one.
+     * <p/>
+     * This ChatListener gets called when this Friend only sends you a message.
+     *
+     * @param message
+     * @param listener Your new active ChatListener
+     */
+    public void sendMessage(String message, ChatListener listener) {
+        this.listener = listener;
+        try {
+            getChat().sendMessage(message);
+        }
+        catch (XMPPException e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * Sets the ChatListener for this friend only. Only 1 ChatListener can be
-	 * active for each Friend. Any previously set ChatListener for this friend
-	 * will be replaced by this one.
-	 *
-	 * This ChatListener gets called when this Friend only sends you a message.
-	 *
-	 * @param listener
-	 */
-	public void setChatListener(ChatListener listener) {
-		this.listener = listener;
-		getChat();
-	}
+    /**
+     * Sets the ChatListener for this friend only. Only 1 ChatListener can be
+     * active for each Friend. Any previously set ChatListener for this friend
+     * will be replaced by this one.
+     * <p/>
+     * This ChatListener gets called when this Friend only sends you a message.
+     *
+     * @param listener
+     */
+    public void setChatListener(ChatListener listener) {
+        this.listener = listener;
+        getChat();
+    }
+
+    public boolean matchesFilterQuery(CharSequence constraint) {
+        return getName().toLowerCase().contains((constraint + "").toLowerCase());
+    }
 }
