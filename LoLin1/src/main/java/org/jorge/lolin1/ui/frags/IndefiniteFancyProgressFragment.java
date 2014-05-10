a58186120ff963019de105e199454cc6ef448d57
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-05-10 15:39:09 +0200

    Added the indefinite progress fragment.

---
+++ LoLin1/src/main/java/org/jorge/lolin1/ui/frags/IndefiniteFancyProgressFragment.java
@@ -0,0 +1,48 @@
+package org.jorge.lolin1.ui.frags;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.jorge.lolin1.R;
+
+/**
+ * This file is part of LoLin1.
+ * <p/>
+ * LoLin1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p/>
+ * LoLin1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with LoLin1. If not, see <http://www.gnu.org/licenses/>.
+ * <p/>
+ * Created by JorgeAntonio on 10/05/2014.
+ */
+public class IndefiniteFancyProgressFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_fancy_indeterminate_progress, container,
+                Boolean.FALSE);
+    }
+}
