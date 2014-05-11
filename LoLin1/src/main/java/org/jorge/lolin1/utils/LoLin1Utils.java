b4b22107c8d743cf2c09357969700592400ba4a0
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-05-11 11:40:20 +0200

    Added a method to check if a service is running.

--- LoLin1/src/main/java/org/jorge/lolin1/utils/LoLin1Utils.java
+++ LoLin1/src/main/java/org/jorge/lolin1/utils/LoLin1Utils.java
@@ -1,6 +1,7 @@
 package org.jorge.lolin1.utils;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -159,6 +160,18 @@ public abstract class LoLin1Utils {
         }
 
         return ret;
+    }
+
+    public static boolean isChatServiceAlreadyRunning(Class serviceClass, Context context) {
+        ActivityManager manager = (ActivityManager) context.getSystemService(
+                Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager
+                .getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Boolean isInternetReachable(Context context) {
