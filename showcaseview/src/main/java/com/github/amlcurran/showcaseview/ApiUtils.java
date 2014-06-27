f6f9ee68bcf2ecf96ba4a257bd942b1ee0de8e4a
Jorge Antonio Diaz-Benito Soriano <jorge.diazbenitosoriano@gmail.com>
2014-06-27 12:00:18 +0200

    Fixed some errors introduced with the new Android L Developer Preview SDK.

--- showcaseview/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
+++ showcaseview/src/main/java/com/github/amlcurran/showcaseview/ApiUtils.java
@@ -1,6 +1,5 @@
 package com.github.amlcurran.showcaseview;
 
-import android.annotation.TargetApi;
 import android.os.Build;
 import android.view.View;
 
@@ -10,11 +9,10 @@ public class ApiUtils {
         return Build.VERSION.SDK_INT >= versionCode;
     }
 
-    public boolean isCompatWithHoneycomb() {
-        return isCompatWith(Build.VERSION_CODES.HONEYCOMB);
-    }
+//    public boolean isCompatWithHoneycomb() {
+//        return isCompatWith(Build.VERSION_CODES.HONEYCOMB);
+//    }
 
-    @TargetApi(14)
     public void setFitsSystemWindowsCompat(View view) {
         if (isCompatWith(Build.VERSION_CODES.ICE_CREAM_SANDWICH)) {
             view.setFitsSystemWindows(true);
